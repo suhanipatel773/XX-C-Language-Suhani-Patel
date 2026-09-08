a7df1e46bf07b40b9e9fcb8c6eb3bb4581323cd3
Suhani patel <suhanipatel773@gmail.com>
2026-09-08 11:52:35 +0530

    Create Hello.c

---
+++ LAB_01/Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  // Standard input-output header
+
+int main() {
+    // Print message to the console
+    printf("Hello, World!\n");
+
+    return 0; // Indicate successful program termination
+}
